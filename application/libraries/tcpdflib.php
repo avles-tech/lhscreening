aa86f8efefe927a548316a2f5afc54612cba7242
smaheshselva <selvakumar@ecyber.com>
2018-11-11 22:08:56 +0530

    rela path

--- application/libraries/tcpdflib.php
+++ application/libraries/tcpdflib.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once dirname(__FILE__) . '/tcpdf/tcpdf.php';
+require_once realpath(dirname(__FILE__) . '/tcpdf/tcpdf.php');
 
 class Tcpdflib extends TCPDF
 {
