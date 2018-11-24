c2a2e43f43b1a38a594d4b5ea870fd837a31b163
smaheshselva <selvakumar@ecyber.com>
2018-11-24 15:17:49 +0530

    upload bit of styling

--- application/views/patients/patient_upload.php
+++ application/views/patients/patient_upload.php
@@ -1,7 +1,7 @@
 <br /><br />
 <div class="form-group">
 <h4>Blood Report</h4>
-	<br /><br />
+	<br />
 	<?php 
 			$patient_reports = $this->patient_reports_model->get_patient_reports($patient_id);
 			//echo form_open_multipart('upload/do_upload_ex');
@@ -10,46 +10,43 @@
 			echo form_hidden('report','blood'); 
 		?>
 	<input type="file" name="userfile" size="20" />
-
 	<input type="submit" value="upload" />
-	</form>
 	<?php if (!empty($patient_reports['blood'])) : ?>
 	<a href = "<?php echo base_url().'index.php/upload/download/'.$patient_reports['blood'] ?>" > download</a>
 	<?php endif?>
+	</form>
 </div> <!-- form-group end.// -->
 <h4>MRI Report</h4>
 <div class="form-group">
-	<br /><br />
+	<br />
 	<?php 
 			echo form_open_multipart('upload/do_upload');
 			echo form_hidden('patient_id',$patient_id); 
 			echo form_hidden('report','mri'); 
 		?>
 	<input type="file" name="userfile" id= 'userfile' size="20" accept=".jpeg,.jpg,.pdf"/>
-
 	<input type="submit" value="upload" />
-	</form>
 	<?php if (!empty($patient_reports['mri'])) : ?>
 	<a href = "<?php echo base_url().'index.php/upload/download/'.$patient_reports['mri'] ?>" > download</a>
 	<?php endif?>
+	</form>
+	
 </div> <!-- form-group end.// -->
 <h4>XRay</h4>
 <div class="form-group">
-	<br /><br />
+	<br />
 	<?php 
 			echo form_open_multipart('upload/do_upload');
 			echo form_hidden('patient_id',$patient_id); 
 			echo form_hidden('report','xray'); 
 		?>
 	<input type="file" name="userfile" size="20" />
-
 	<input type="submit" value="upload" />
-	</form>
 	<?php if (!empty($patient_reports['xray'])) : ?>
 	<a href = "<?php echo base_url().'index.php/upload/download/'.$patient_reports['xray'] ?>" > download</a>
 	<?php endif?>
+	</form>
 </div> <!-- form-group end.// -->
-
 <?php 
 //$this->load->view('patients/patient_report',array( 'patient_id' => $patient_id)); 
 ?>
