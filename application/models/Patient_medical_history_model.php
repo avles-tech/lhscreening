8c14e5c6196d7fae4297e01a10408b347cde8679
smaheshselva <selvakumar@ecyber.com>
2018-12-11 23:23:37 +0530

    travel partial

--- application/models/Patient_medical_history_model.php
+++ application/models/Patient_medical_history_model.php
@@ -15,7 +15,11 @@ class Patient_medical_history_model extends CI_Model {
         public function set($patient_id,$form_data)
         {
                 $this->load->helper('url');
-    
+
+                $travel = $form_data['travel'];
+
+                unset($form_data['travel']);;
+
                 $query = $this->db->get_where('patient_medical_history', array('patient_id' => $patient_id));
                 $patient_gp = $query->result_array();
                 $count = count($patient_gp);
@@ -24,6 +28,14 @@ class Patient_medical_history_model extends CI_Model {
                         return $this->db->insert('patient_medical_history', $form_data);
                 }
                 else{
+                        
+                        $this->db->delete('patient_travel', array('patient_id' => $patient_id));
+                        foreach ($travel as $value):
+                                //$value['patient_id'] = $patient_id;
+                                echo $travel['travel_destination'];
+                                //$this->db->insert('patient_travel', $value);
+                        endforeach;
+
                         $this->db->where('patient_id' , $patient_id);
                         return $this->db->update('patient_medical_history', $form_data);
                 }
